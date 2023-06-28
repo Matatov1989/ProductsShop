43ca8f41bbcaaf1b1f4b8b6ebfbf1fa0e98ac532
Yury <matatov1989@gmail.com>
2023-06-28 17:30:44 +0300

    inject repository

--- app/src/main/java/com/example/productsshop/fragments/NumbersViewModel.kt
+++ app/src/main/java/com/example/productsshop/fragments/NumbersViewModel.kt
@@ -1,8 +1,24 @@
 package com.example.productsshop.fragments
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.productsshop.repository.ProductsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import javax.inject.Inject
 
 @HiltViewModel
-class NumbersViewModel : ViewModel(){
+class NumbersViewModel @Inject constructor(private val repository: ProductsRepository) : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+            try {
+                val result = repository.getProducts()
+            } catch (e: Exception) {
+                Log.e("RESULT_EXCEPTION", "result: $e")
+            }
+        }
+    }
 }
