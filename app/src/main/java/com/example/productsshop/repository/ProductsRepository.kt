b1ee8c13516f53ccaf8fa579c0c0a9dc7502f2a7
Yury <matatov1989@gmail.com>
2023-06-28 17:24:13 +0300

    inject api

--- app/src/main/java/com/example/productsshop/repository/ProductsRepository.kt
+++ app/src/main/java/com/example/productsshop/repository/ProductsRepository.kt
@@ -1,4 +1,13 @@
 package com.example.productsshop.repository
 
-class ProductsRepository {
+import com.example.productsshop.models.ProductsResponse
+import com.example.productsshop.network.ProductsApi
+import retrofit2.Response
+import javax.inject.Inject
+
+class ProductsRepository @Inject constructor(private val api: ProductsApi) {
+
+    suspend fun getProducts(): Response<ProductsResponse> {
+        return api.getProducts()
+    }
 }
