aae5bccf6ce583edec4df0686cea22e17b6aebfe
Yury <matatov1989@gmail.com>
2023-06-29 14:05:36 +0300

    create product information fragment

---
+++ app/src/main/java/com/example/productsshop/fragments/ProductInfoFragment.kt
@@ -0,0 +1,34 @@
+package com.example.productsshop.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.productsshop.R
+import com.example.productsshop.databinding.FragmentProductInfoBinding
+
+
+class ProductInfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentProductInfoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProductInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
